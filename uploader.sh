6ebf5e00c1895838c7fabf454e035e7c467779a7
ivan-gars <w.garusov.ivan@gmail.com>
2025-11-26 16:08:42 +0200

    address_change

--- uploader.sh
+++ uploader.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-HEX_DIR=~/Documents/PlatformIO/Projects/Adafruit_nRF52_Bootloader/_bin/xiao_nrf52840_ble_sense
+HEX_DIR=./_bin/xiao_nrf52840_ble_sense
 LATEST_HEX=$(find "$HEX_DIR" -type f -name '*s140*.hex' -printf '%T@ %p\n' | sort -nr | head -n1 | cut -d' ' -f2-)
 
 if [ -z "$LATEST_HEX" ]; then
@@ -10,4 +10,4 @@ fi
 
 echo "$LATEST_HEX"
 
-openocd -f ~/stlink_nrf52.cfg -c "init; halt; flash write_image erase \"$LATEST_HEX\" 0x26000 verify reset exit"    #set WORKAREASIZE 0 or set WORKAREASIZE 0x400 in config
+openocd -f ~/stlink_nrf52.cfg -c "init; halt; flash write_image erase \"$LATEST_HEX\" 0x00000000 verify reset exit"    #set WORKAREASIZE 0 or set WORKAREASIZE 0x400 in config
